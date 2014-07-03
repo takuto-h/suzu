a48d402e23be5c2a4d761fdf0640f7f728888af5
Takuto Hayashi <takuto.hayashi@gmail.com>
2014-07-03 16:23:25 +0900

    Add echo repl

--- main.ml
+++ main.ml
@@ -1,2 +1,30 @@
 
-let () = Printf.printf "hello, suzu.\n"
+open Printf
+
+let initial_buffer_size = 256
+
+let rec read buf =
+  let line = read_line () in
+  if line = "" then
+    Buffer.contents buf
+  else
+    begin
+      Buffer.add_string buf (sprintf "%s\n" line);
+      printf "... ";
+      read buf
+    end
+
+let rec loop () =
+  begin try
+    begin
+      printf ">>> ";
+      let str = read (Buffer.create initial_buffer_size) in
+      printf "%s\n" str;
+      loop ()
+    end
+  with
+    | End_of_file ->
+      ()
+  end
+
+let () = loop ()
