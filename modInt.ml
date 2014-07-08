058da88003cf27e36addac82b1fc01be9973e36a
Takuto Hayashi <takuto.hayashi@gmail.com>
2014-07-08 21:16:57 +0900

    Add operators to ModInt

--- modInt.ml
+++ modInt.ml
@@ -1,15 +1,41 @@
 
+let binary_cmp_op proc =
+  Value.Subr begin 2, fun pos args ->
+    let self = List.nth args 0 in
+    let i = List.nth args 1 in
+    Value.Bool (proc (Eva.int_of_value pos self) (Eva.int_of_value pos i))
+  end
+
+let binary_arith_op proc =
+  Value.Subr begin 2, fun pos args ->
+    let self = List.nth args 0 in
+    let i = List.nth args 1 in
+    Value.Int (proc (Eva.int_of_value pos self) (Eva.int_of_value pos i))
+  end
+
+let unary_arith_op proc =
+  Value.Subr begin 1, fun pos args ->
+    let self = List.nth args 0 in
+    Value.Int (proc (Eva.int_of_value pos self))
+  end
+
 let initialize env =
   let mod_int = Value.Env.create_local env in
   begin
     Value.Env.add_var env "Int" (Value.Module mod_int);
     Value.Env.add_var mod_int "answer" (Value.Int 42);
     Value.Env.add_var mod_int "C" (Value.Class "Int:C");
-    Value.Env.add_method env "Int:C" "+" begin
-      Value.Subr begin 2, fun pos args ->
-        let self = List.nth args 0 in
-        let i = List.nth args 1 in
-        Value.Int (Eva.int_of_value pos self + Eva.int_of_value pos i)
-      end
-    end
+    Value.Env.add_method env "Int:C" "<" (binary_cmp_op ( < ));
+    Value.Env.add_method env "Int:C" ">" (binary_cmp_op ( > ));
+    Value.Env.add_method env "Int:C" "<=" (binary_cmp_op ( <= ));
+    Value.Env.add_method env "Int:C" ">=" (binary_cmp_op ( >= ));
+    Value.Env.add_method env "Int:C" "==" (binary_cmp_op ( = ));
+    Value.Env.add_method env "Int:C" "!=" (binary_cmp_op ( <> ));
+    Value.Env.add_method env "Int:C" "+" (binary_arith_op ( + ));
+    Value.Env.add_method env "Int:C" "-" (binary_arith_op ( - ));
+    Value.Env.add_method env "Int:C" "*" (binary_arith_op ( * ));
+    Value.Env.add_method env "Int:C" "/" (binary_arith_op ( / ));
+    Value.Env.add_method env "Int:C" "%" (binary_arith_op ( mod ));
+    Value.Env.add_method env "Int:C" "~+" (unary_arith_op ( ~+ ));
+    Value.Env.add_method env "Int:C" "~-" (unary_arith_op ( ~- ));
   end
