eacea7ac5b3fc62672c661d98d7895e7452b50f8
Takuto Hayashi <takuto.hayashi@gmail.com>
2014-07-22 04:14:20 +0900

    Enable to load source files

--- main.ml
+++ main.ml
@@ -1,6 +1,13 @@
 
+open Printf
+
+let usage_msg = sprintf "usage: %s [source_files]" Sys.argv.(0)
+
 let main () =
   let interp = Interp.create () in
+  Arg.parse_argv ~current:(ref 0) Sys.argv [] begin fun file_name ->
+    Interp.load_file interp file_name
+  end usage_msg;
   Interp.repl interp
 
 let () = main ()
