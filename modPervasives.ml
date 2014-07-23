dd303a9060b8ac6f0a2a5ebce0e5c8af1a64af2c
Takuto Hayashi <takuto.hayashi@gmail.com>
2014-07-23 20:34:25 +0900

    Rename ModPervasives.*

--- modPervasives.ml
+++ modPervasives.ml
@@ -1,12 +1,12 @@
 
-let write_line =
+let subr_write_line =
   Eva.Subr begin 1, true, fun eva pos args ->
       let str = Eva.call_fun eva pos ModString.subr_string_format args in
       print_endline (Eva.string_of_value pos str);
       Eva.Unit
   end
 
-let read_line =
+let subr_read_line =
   Eva.Subr begin 0, false, fun eva pos args ->
       Eva.String (read_line ())
   end
@@ -14,6 +14,6 @@ let read_line =
 let initialize env =
   let mod_pervasives = Eva.Env.create_local env in
   Eva.Env.add_var env "Pervasives" (Eva.Module mod_pervasives);
-  Eva.Env.add_var mod_pervasives "write_line" write_line ~export:true;
-  Eva.Env.add_var mod_pervasives "read_line" read_line ~export:true;
+  Eva.Env.add_var mod_pervasives "write_line" subr_write_line ~export:true;
+  Eva.Env.add_var mod_pervasives "read_line" subr_read_line ~export:true;
   Eva.Env.open_module env mod_pervasives
